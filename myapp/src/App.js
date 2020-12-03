e294da920f47306d22896124b368ce388b265714
karthi <karthi@exm.com>
2020-12-03 22:47:52 +0530

    conditional output

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,23 @@
+import React, { Component } from 'react';
+import Ninjas from './ninjas';
 
-
-function App() {
+class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'karma', age: 25, belt: 'pink', id: 3 }
+    ]
+  }
+  render() {
   return (
     <div className="App">
       <h1>My first React app</h1>
       <p>Welcome :)</p>
+      <Ninjas ninjas={this.state.ninjas}/>
     </div>
   );
+ }
 }
 
 export default App;
