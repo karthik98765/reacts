0e402c45602d14ccbaa26a8e5bbcc243969ea33b
karthi <karthi@exm.com>
2020-12-04 14:21:46 +0530

    function as props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
   state = {
@@ -9,12 +10,20 @@ class App extends Component {
       { name: 'karma', age: 25, belt: 'pink', id: 3 }
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
   render() {
   return (
     <div className="App">
       <h1>My first React app</h1>
       <p>Welcome :)</p>
-      <Ninjas ninjas={this.state.ninjas}/>
+      <Ninjas ninjas={this.state.ninjas} />
+      <AddNinja addNinja={this.addNinja} />
     </div>
   );
  }
